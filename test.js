254cb1d953a3f702eb9a6b70d1519914be2b1ad8
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-13 18:14:25 +0200

    extend tests

--- test.js
+++ test.js
@@ -2,19 +2,13 @@ import { deepEqual } from 'assert';
 import mergeConcat from './index';
 
 it('should mergeConcat', () =>
-  deepEqual(mergeConcat({
-    hey: 'hey',
-    list: [1, 2]
-  }, {
-    ho: 'ho',
-    list: [3, 4]
-  },
-  {
-    uni: 'corns'
-  }
-), {
-  hey: 'hey',
-  ho: 'ho',
-  uni: 'corns',
-  list: [1, 2, 3, 4]
-}));
+  deepEqual(mergeConcat(
+      { hey: 'hey', list: [1, 2] },
+      { ho: 'ho', list: [3, 4] },
+      { uni: 'corns' }
+    ),
+    { hey: 'hey', ho: 'ho', uni: 'corns', list: [1, 2, 3, 4] }
+  ));
+
+it('should mergeConcat with empty input', () =>
+  deepEqual(mergeConcat(), {}));
