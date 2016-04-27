8478d87a275b7a41851b9d102c2a2a6c8220c501
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-27 11:25:05 +0200

    rewrite tests

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
 import test from 'ava';
-import { mergeAndConcat, mergeAndConcatAsync } from './index';
+import mergeAndConcat from './index';
 
-test('basic', t =>
-  t.is(mergeAndConcat('unicorns'), 'unicorns'));
+test('basic', t => t.deepEqual(
+  mergeAndConcat(
+    { hey: 'hey', list: [1, 2] },
+    { ho: 'ho', list: [3, 4] },
+    { uni: 'corns' }
+  ),
+  { hey: 'hey', ho: 'ho', uni: 'corns', list: [1, 2, 3, 4] }
+));
+
+test('remove dupes', t => t.deepEqual(
+  mergeAndConcat(
+    { after_script: ['npm run coverage'] },
+    { after_script: ['npm run coverage'] }
+  ),
+  { after_script: ['npm run coverage'] }
+));
 
 test('empty input', t => t.throws(() => { mergeAndConcat(); }, TypeError));
 test('invalid input', t => t.throws(() => { mergeAndConcat(2); }, TypeError));
-
-test('async :: basic', async t => t.is(
-  await mergeAndConcatAsync('unicorns'),
-  'unicorns'));
-
-test('async :: empty input', t => t.throws(mergeAndConcatAsync(), TypeError));
-test('async :: invalid input', t => t.throws(mergeAndConcatAsync(2), TypeError));
